bc97aaacd2d6e45c73ef4a6add94a9a06b8f668e
doug <drcox23@gmail.com>
2018-09-26 20:51:47 -1000

    hello world for initial get route

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const knex = require('./db/knex.js')
+
+const PORT = process.env.EXPRESS_CONTAINER_PORT;
+// const Tasks = require('./db/models/Tasks');
+// const Users = require('./db/models/Users');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.send('sanity check');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`)
+})
