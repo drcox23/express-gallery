838c8918414b80abc41906ef1e3b3dfcc5766712
JEFrady <jamie.e.halliday@gmail.com>
2018-09-27 20:16:23 -1000

    changes accept

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(bodyParser.urlencoded({
 
 // GET HOME
 app.get('/', (req, res) => {
-<<<<<<< HEAD
   res.sendfile('./index.html')
   // res.send('sanity check')
 })
@@ -103,108 +102,6 @@ app.get('/', (req, res) => {
 //       res.json(err);
 //     })
 // })
-=======
-  res.send('sanity check')
-  console.log('we have a connection')
-})
-
-// get all users
-app.get('/api/users', (req, res) => {
-  Users
-    .fetchAll()
-    .then(users => {
-      res.json(users.serialize());
-    })
-    .catch(err => {
-      res.json(err);
-    })
-})
-
-// get all photos by user_id
-app.get('/api/users/:user_id/photos', (req, res) => {
-  const {
-    user_id
-  } = req.params;
-  Photos
-    .where({
-      user_id
-    })
-    .fetchAll()
-    .then(photos => {
-      res.json(photos.serialize())
-    })
-    .catch(err => {
-      res.json(err);
-    })
-})
-
-// create task by user id
-app.post('/api/users/:user_id/photos/new', (req, res) => {
-  const {
-    user_id
-  } = req.params;
-  const payload = {
-    name: req.body.name
-  }
-  Photos
-    .forge(payload)
-    .save()
-    .then(result => {
-      res.json(result)
-    })
-    .catch(err => {
-      console.log('error', err)
-      res.json(err);
-    })
-})
-
-// update task by task id
-app.put('/api/photos/:task_id/edit', (req, res) => {
-  const {
-    task_id
-  } = req.params;
-
-  const payload = {
-    name: req.body.name,
-    is_complete: req.body.is_complete
-  }
-
-  Photos
-    .where({
-      task_id
-    })
-    .fetch()
-    .then(task => {
-      return task.save(payload)
-    })
-    .then(result => {
-      console.log('result', result)
-      res.json(result);
-    })
-    .catch(err => {
-      res.json(err);
-    })
-})
-
-// delete task by task id
-app.delete('/api/photos/:photo_id/delete', (req, res) => {
-  const {
-    task_id
-  } = req.params;
-
-  Photos
-    .where({
-      task_id
-    })
-    .destroy()
-    .then(result => {
-      res.json(result);
-    })
-    .catch(err => {
-      res.json(err);
-    })
-})
->>>>>>> seeds
 
 
 app.listen(PORT, () => {
